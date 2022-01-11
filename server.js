0b8fe07f677b64da06b0104d8b84922988fe7b65
Shrey Thakkar <shrey.thakkar@smartsensesolutions.com>
2022-01-11 17:25:43 +0530

    Add todo mode and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 const { query } = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -43,14 +44,23 @@ app.get('/todos/:id',function(req, res){
 
 app.post('/todos', function(req, res){
     var body = _.pick(req.body, 'description', 'completed');
-    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
-        return res.status(400).send();
-    };
-    body.description = body.description.trim();
-    body.id = todoNextId++;
-    todos.push(body);
-    // console.log('description: ' + body.description);
-    res.json(body);
+    db.todo.create(body).then(function(todo){
+        res.json(todo.toJSON());
+    }, function(e){
+        res.status(400).json(e);
+    });
+
+
+
+
+    // if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+    //     return res.status(400).send();
+    // };
+    // body.description = body.description.trim();
+    // body.id = todoNextId++;
+    // todos.push(body);
+    // // console.log('description: ' + body.description);
+    // res.json(body);
 });
 
 app.delete('/todos/:id', function(req, res){
@@ -88,7 +98,8 @@ app.put('/todos/:id', function(req, res){
     _.extend(matchedTodo, validAttributes);
     res.json(matchedTodo);
 });
-
-app.listen(PORT, function(){
-    console.log('Express listening on port' + PORT + ' !');
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function(){
+        console.log('Express listening on port' + PORT + ' !');
+    });
 });
