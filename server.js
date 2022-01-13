c6beaeaa649166d93cabf30ff557abb372f0f551
Shrey Thakkar <shrey.thakkar@smartsensesolutions.com>
2022-01-13 12:15:40 +0530

    Convert CRUD request to user current user todos only

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ app.get('/',function (req, res){
 
 app.get('/todos', middleware.requireAuthentication, function(req, res){
     var query = req.query;
-    var where = {};
+    var where = {
+        userId: req.user.get('id')
+    };
     if (query.hasOwnProperty('completed') && query.completed === 'true') {
         where.completed = true;        
     } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
@@ -56,7 +58,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res){
 app.get('/todos/:id',middleware.requireAuthentication,function(req, res){
     var todoId = parseInt(req.params.id, 10);
 
-    db.todo.findById(todoId).then(function (todo){
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+        }
+    }).then(function (todo){
         if (!!todo) {
             res.json(todo.toJSON());
         } else{
@@ -107,7 +114,8 @@ app.delete('/todos/:id',middleware.requireAuthentication, function(req, res){
 
     db.todo.destroy({
         where: {
-            id: todoId
+            id: todoId,
+            userId: req.user.get('id')
         }
     }).then(function(rowsDeleted){
         if (rowsDeleted === 0 ) {
@@ -144,7 +152,12 @@ app.put('/todos/:id',middleware.requireAuthentication, function(req, res){
         attributes.description = body.description;        
     } 
 
-    db.todo.findById(todoId).then(function (todo){
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+        }
+    }).then(function (todo){
         if (todo) {
             todo.update(attributes).then(function (todo){
                 res.json(todo.toJSON());
